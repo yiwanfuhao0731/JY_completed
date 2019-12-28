12644d87da21bc08f101fef179fb4d1bcc0e25cd
yiwanfuhao <yangzixiao001@gmail.com>
2019-12-28 10:47:10 +0000

    Create portfolio_RV_vol.py
    risk budgeting framework for RV

---
+++ Caxton/strategy/portfolio_RV_vol.py
@@ -0,0 +1,8 @@
+# @justinY 20191228
+# this file is to lift up for the RV system strategy the volatility to a target
+# input would be the signals, instrument durations, 5 year swap rate,
+
+from Analytics.abstract_sig import abstract_sig_genr_portfolio as abs_sig
+
+class signal3(abs_sig):
+    

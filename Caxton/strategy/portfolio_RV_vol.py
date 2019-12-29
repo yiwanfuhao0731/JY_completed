32f8d0ce983fb26136e9f63a0a8f12ee6ba87404
yiwanfuhao <yangzixiao001@gmail.com>
2019-12-29 19:40:18 +0000

    adding risk budgeting of RV
    adding risk budgeting of RV

--- Caxton/strategy/portfolio_RV_vol.py
+++ Caxton/strategy/portfolio_RV_vol.py
@@ -2,7 +2,15 @@
 # this file is to lift up for the RV system strategy the volatility to a target
 # input would be the signals, instrument durations, 5 year swap rate,
 
-from Analytics.abstract_sig import abstract_sig_genr_portfolio as abs_sig
+from strategy.Analytics_for_pc.abs_sig_genr import abstract_sig_genr_portfolio as abs_sig
 
 class signal3(abs_sig):
-    
+    def __init__(self):
+        super(signal3, self).__init__()
+
+    # the target is to simply lift the RV strategy to 4% target vol
+    def run_rb_constant_main(self,*args,**kwargs):
+        pass
+
+    def run_rb_constant_fund_scale(self,*args,**kwargs):
+        pass

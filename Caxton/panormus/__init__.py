f8abc7f7832dbf7ec32280efa61ffe144395ebe9
yiwanfuhao0731 <53380447+yiwanfuhao0731@users.noreply.github.com>
2020-11-12 13:09:56 +0000

    updating EMFX

--- Caxton/panormus/__init__.py
+++ Caxton/panormus/__init__.py
@@ -1,1 +1,11 @@
 import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
+import sys
+import os
+print ('hello world')
+
+
+output_filename = r'C:\yang\Third_yr_grad_English\relevant_file\bookkeeping\tax return\2019-2020\recept.zip'
+dir_name = r'C:\yang\Third_yr_grad_English\relevant_file\bookkeeping\tax return\2019-2020\receipt'
+import shutil
+shutil.make_archive(output_filename, 'zip', dir_name)
